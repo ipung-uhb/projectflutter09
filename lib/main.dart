6612ac1eacc113411f32fd13064abe733b1fd7bf
ipung-uhb <purwono@uhb.ac.id>
2023-11-20 10:09:41 +0700

    Instalasi Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo Apps New',
+      title: 'Flutter Demo Apps New Oke',
       theme: ThemeData(
         // This is the theme of your application.
         //
